248e43df44b4293f6d47f846481f514d1d32f27e
Billy Jobless <granthampie@gmail.com>
2023-11-29 15:45:01 +0000

    initial check in

---
+++ versions.tf
@@ -0,0 +1,14 @@
+#Terraform Block
+terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.82.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.5.1"
+    }
+  }
+}
